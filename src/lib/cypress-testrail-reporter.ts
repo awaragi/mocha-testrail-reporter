736545a94270d7db9e53675f6fb73b7aa86c2644
tanakaworld <yutaro.tanaka.world@gmail.com>
2020-11-01 11:54:41 +0900

    feat: set password via runtime env var

--- src/lib/cypress-testrail-reporter.ts
+++ src/lib/cypress-testrail-reporter.ts
@@ -13,6 +13,11 @@ export class CypressTestRailReporter extends reporters.Spec {
     super(runner);
 
     let reporterOptions = options.reporterOptions;
+
+    if (process.env.CYPRESS_TESTRAIL_REPORTER_PASSWORD) {
+      reporterOptions.password = process.env.CYPRESS_TESTRAIL_REPORTER_PASSWORD;
+    }
+
     this.testRail = new TestRail(reporterOptions);
     this.validate(reporterOptions, 'domain');
     this.validate(reporterOptions, 'username');
