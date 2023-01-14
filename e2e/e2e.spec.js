8a12765f5e10ef4fe9432bda07efbb2fadd75cec
Pierre Awaragi <pierre.awaragi@cgi.com>
2023-01-14 11:26:05 -0500

    added users listing fetch

--- e2e/e2e.spec.js
+++
@@ -1,7 +0,0 @@
-describe("E2E Testrail Mocha Reporter", () => {
-  it("C1 Test case 1", () => {});
-  it("C2 Test case 1", () => {});
-  it("C3 Test case 1", () => {
-    throw new Error("Failed test");
-  });
-});
--- index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("./dist/lib/mocha-testrail-reporter").MochaTestRailReporter
